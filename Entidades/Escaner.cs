71249bff254b6eb6f507c37c577c2c65c4704ffa
Alejo Podbielski <124207059+Valentinpodbielski@users.noreply.github.com>
2024-05-22 14:52:38 -0300

    Update Escaner.cs

--- Entidades/Escaner.cs
+++ Entidades/Escaner.cs
@@ -1,6 +1,9 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,94 +11,139 @@ namespace Entidades
 {
     public class Escaner
     {
-        //  Campos  //
-        private List<Documento> listaDocumentos;
-        private Departamento locacion;
-        private string marca;
-        private TipoDoc tipo;
-
-
-        // Constructor //
-        public Escaner(string marca, TipoDoc tipo)
-        {
-            this.marca = marca;
-            this.tipo = tipo;
-            this.listaDocumentos = new List<Documento>(); // Inicializa la lista de documentos
-
-            // Inicializa la locacion segun el tipo de documento a escanear
-            this.locacion = (tipo == TipoDoc.mapa) ? Departamento.mapoteca : Departamento.procesosTecnicos;
-            
-        }
-
-
-        //  Propiedades  //
-        public List<Documento> ListaDocumentos
-        {
-            get { return listaDocumentos; }
-        }
-        public Departamento Locacion
-        {
-            get { return locacion; }
-        }
-        public string Marca
-        {
-            get { return marca; }
-        }
-        public TipoDoc Tipo
-        {
-            get { return tipo; }
-        }
-
-
-        //  Metodos  //
-        public static bool operator ==(Escaner e, Documento d)
-        {
-            return e.listaDocumentos.Any(doc => doc == d);
-        }
-
-        public static bool operator !=(Escaner e, Documento d)
-        {
-            return !(e == d);
-        }
-
-        public static bool operator +(Escaner e, Documento d)
-        {
-            // Para que no haya error el escaner y el documento tienen que ser iguales, para no guardar un libro en mapas
-            if (e.Tipo == TipoDoc.libro && d is Libro || e.Tipo == TipoDoc.mapa && d is Mapa)
-            {
-                if (e != d && d.Estado == Documento.Paso.Inicio) // Si el documento no se encuentra en el escaner y esta en el inicio
-                {
-                    e.CambiarEstadoDocumento(d); // Avanza un estado
-                    e.listaDocumentos.Add(d); // Y lo mete en la lista de documentos
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public bool CambiarEstadoDocumento(Documento d)
-        {
-            if (listaDocumentos.Contains(d)) // Si el documento se encuentra en la lista continua
-            {
-                return d.AvanzarEstado(); // Llama al metodo AvanzarEstado para pasar al siguiente estado
-            }
-            return false;
-        }
-
-
-        //  Propiedades  //
         public enum Departamento
         {
             nulo,
             mapoteca,
             procesosTecnicos
         }
+
         public enum TipoDoc
         {
             libro,
             mapa
         }
+        Departamento locacion;
+        string marca;
+        TipoDoc tipo;
+        List<Documento> listaDocumentos;
+
+
+
+        // Propiedades  //
+
+        public List<Documento> ListaDocumentos
+        {
+            get { return listaDocumentos; }
+        }
+
+        public Departamento Locacion
+        {
+            get { return locacion; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+        public TipoDoc Tipo
+        {
+            get { return tipo; }
+        }
+
+        // Metodos  //
      
+        public bool CambiarEstadoDocumento(Documento d)
+        {
+            return d.AvanzarEstado();
+        }
+
+        public Escaner(string marca, TipoDoc tipo)
+        {
+            this.marca = marca;
+            this.tipo = tipo;
+            this.listaDocumentos = new List<Documento>(); // Intanciamos una lista
+
+            switch (this.tipo) // Hacemos un switch para elegir el tipo
+            {
+                case TipoDoc.libro:
+                    locacion = Departamento.procesosTecnicos;
+                    break;
+                case TipoDoc.mapa:
+                    locacion = Departamento.mapoteca;
+                    break;
+                default:
+                    locacion = Departamento.nulo;
+                    break;
+            }
+        }
+
+
+
+        public static bool operator ==(Escaner e, Documento d) // averigua si el documento esta en el escaner
+        {
+            bool retorno;
+            retorno = false;
+            if (e.tipo == TipoDoc.libro)
+            {
+                try
+                {
+                    foreach (Libro doc in e.listaDocumentos)
+                    {
+                        if (doc == (Libro)d)
+                        {
+                            retorno = true;
+                        }
+                    }
+                }
+                catch (InvalidCastException excepcion)
+                {
+                    Console.WriteLine(excepcion.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    foreach (Mapa doc in e.listaDocumentos)
+                    {
+                        if (doc == (Mapa)d)
+                        {
+                            retorno = true;
+
+                        }
+                    }
+                }
+                catch (InvalidCastException excepcion)
+                {
+                    Console.WriteLine(excepcion.Message);
+                }
+            }
+            return retorno;
+
+        }
+
+        public static bool operator !=(Escaner e, Documento d)
+        {
+            return !(e == d); // Retorna un false
+        }
+
+        
+
+        public static bool operator +(Escaner e, Documento d)
+        {
+            if (e.tipo == TipoDoc.libro && d is Libro || e.tipo == TipoDoc.mapa && d is Mapa)
+            {
+                if (e != d && d.Estado == Documento.Paso.Inicio)
+                {
+                    e.CambiarEstadoDocumento(d); // Avanza un estado
+                    e.listaDocumentos.Add(d); // Adhiere el documento en la lista
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
 
